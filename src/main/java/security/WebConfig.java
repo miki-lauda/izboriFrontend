6ec308b74de6a6f03cbaa3f78a5eb18730d01697
Milan Marinkovic <milan_marinkovic98@hotmail.com>
2020-09-17 15:15:26 +0200

    c

--- src/main/java/security/WebConfig.java
+++ src/main/java/security/WebConfig.java
@@ -7,6 +7,6 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/glasac/dodajGlasaca").allowedOrigins("https://tender-ride-cfdf82.netlify.app").allowedMethods("POST");
+		registry.addMapping("/**").allowedOrigins("/**");
 	}
 }
