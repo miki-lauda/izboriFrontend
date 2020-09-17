d5257100145d7b056807f90786d30e1a405d0ad8
Milan Marinkovic <milan_marinkovic98@hotmail.com>
2020-09-17 12:48:40 +0200

    dvds

---
+++ src/main/java/controller/GlasacController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import model.Glasac;
+
+@RestController
+@RequestMapping(value = "glasac")
+public class GlasacController {
+
+	@PutMapping(value="/dodajGlasaca",consumes = "application/json")
+	public ResponseEntity<Boolean> dodajGlasaca(@RequestBody Glasac glasac){
+		System.out.println(glasac.toString());
+		return new ResponseEntity<>(true,HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/dodajGlasaca")
+	public ResponseEntity<Boolean> dodajGlasaca(){
+		System.out.println("fgfdgfgdfgfdgdf");
+		return new ResponseEntity<>(true,HttpStatus.OK);
+	}
+}
