918a9396d0033a48e58e70c58f704428b0035e4b
Milan Marinkovic <milan_marinkovic98@hotmail.com>
2020-09-17 15:08:54 +0200

    aa

--- src/main/java/bekend/demo/DemoApplication.java
+++ src/main/java/bekend/demo/DemoApplication.java
@@ -19,16 +19,4 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
-	@Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://tender-ride-cfdf82.netlify.app"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
-                "Cache-Control", "Content-Type"));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
--- src/main/java/security/WebConfig.java
+++ src/main/java/security/WebConfig.java
@@ -7,6 +7,6 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("/**");
+		registry.addMapping("/glasac/dodajGlasaca").allowedOrigins("https://tender-ride-cfdf82.netlify.app").allowedMethods("POST");
 	}
 }
