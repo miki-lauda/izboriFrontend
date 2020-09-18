6f5f44463a985a1b3122ba621a790ae72f2e0f3f
Milan Marinkovic <milan_marinkovic98@hotmail.com>
2020-09-18 14:08:18 +0200

    ffg

---
+++ WebApplication4/Controllers/GlasacController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication4.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Newtonsoft.Json;
+
+namespace WebApplication4.Controllers
+{
+    public class GlasacController : ApiController
+    {
+
+        public HttpResponseMessage Get()
+        {
+            DataTable tabela = new DataTable(tableName:"TabelaGlasaci");
+
+            string query = "select * from dbo.Glasac";
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["GlasaciDB"].ConnectionString)) 
+            using (var cmd = new SqlCommand(query, connection))
+                using(var da=new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(tabela);
+                string JSONresult;
+                JSONresult = JsonConvert.SerializeObject(tabela);
+                return Request.CreateResponse(HttpStatusCode.OK, JSONresult);
+            }
+           
+        }
+
+        public Boolean Post(Glasac glasac)
+        {
+            try
+            {
+                DataTable tabela = new DataTable();
+
+                string query = "insert into dbo.Glasac(ime,prezime,imeOca,izbornaJedinica,iznosPlacanja,glasDobavio,datumRodjenja) values('"+glasac.Ime+"','"+glasac.Prezime+ "','" + glasac.ImeOca + "','" + glasac.IzbornaJedinica + "'," + glasac.IznosPlacanja + ",'" + glasac.GlasDobavio + "','" + glasac.DatumRodjenja + "')";
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["GlasaciDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, connection))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(tabela);
+                }
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ WebApplication4/Models/Glasac.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Glasac
+    {
+        public long Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string ImeOca { get; set; }
+        public string DatumRodjenja { get; set; }
+        public int IznosPlacanja { get; set; }
+        public string IzbornaJedinica { get; set; }
+        public string GlasDobavio { get; set; }
+    }
+}
